e86a2f38bcb1ed7b97f18fdcdc5e3e1e1bd9f1e1
Jacob <jacob.echeverry13@gmail.com>
2024-10-26 23:25:41 -0400

    added console logs for change of text + fixes of text area

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,3 +1,14 @@
 export default function TextArea() {
-  return <textarea className="textarea" />;
+  return (
+    <textarea
+      onChange={(e) => {
+        console.log("I'm changing!");
+        // e. target is the text area, we are getting whats written in there
+        console.log(e.target.value);
+      }}
+      className="textarea"
+      placeholder="Enter your text here"
+      spellCheck="false"
+    />
+  );
 }
