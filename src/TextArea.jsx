5884d3229b289f639732c4ff53d3bf3627ecef4d
Jacob <jacob.echeverry13@gmail.com>
2024-10-26 23:51:56 -0400

    added controlled component for text area

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 export default function TextArea() {
+  // text is the state variable, what it begins with 
+  const [text, setText] = useState("");
+
   return (
     <textarea
+      //uses text to initialize the value of the textarea
+      // empty string
+      value={text}
+      // we are making this into a controlled component
       onChange={(e) => {
-        console.log("I'm changing!");
-        // e. target is the text area, we are getting whats written in there
-        console.log(e.target.value);
+        // we are updating the state of the text, with value of text area
+        // runs when any text is inputted
+        const newText = e.target.value;
+        setText(newText);
       }}
       className="textarea"
       placeholder="Enter your text here"
