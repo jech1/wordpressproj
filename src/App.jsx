bb84480cfd303754c54451d95c95c619d17017c6
Jacob <jacob.echeverry13@gmail.com>
2024-10-26 21:20:25 -0400

    updated imports and exports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Container from "./Container";
+import Header from "./Header";
+import Footer from "./Footer";
+
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
