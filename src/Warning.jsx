011dabb2ba76370ee4062e1abea70782a4de2343
Jacob <jacob.echeverry13@gmail.com>
2024-10-27 20:39:08 -0400

    comments

--- src/Warning.jsx
+++ src/Warning.jsx
@@ -1,3 +1,6 @@
+// here we are creating a functional component called Warning
+// this component takes in a prop called warningText
+// the prop is then displayed in a p tag with a class of warning
 export default function Warning({ warningText }) {
   return <p className="warning">{warningText}</p>;
 }
