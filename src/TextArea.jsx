6416f2056e6f5c25787aadd8d8549604ccb2fc23
Jacob <jacob.echeverry13@gmail.com>
2024-10-27 20:07:31 -0400

    added pevention variables and cleaned up our return

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -3,6 +3,21 @@ import { useState } from "react";
 export default function TextArea() {
   // text is the state variable, what it begins with
   const [text, setText] = useState("");
+  // we are making this into a controlled component
+  const handleChange = (e) => {
+    // we are updating the state of the text, with value of text area
+    // runs when any text is inputted
+    console.log("onChange event fired");
+    console.log(e.target.value);
+    let newText = e.target.value;
+    // in order to prevent script tags from being entered
+    if (newText.includes("<script>")) {
+      alert("You cannot use script tags");
+      //this replaces the script tag with an empty string
+      newText = newText.replace("<script>", "");
+    }
+    setText(newText);
+  };
 
   return (
     <textarea
@@ -10,14 +25,7 @@ export default function TextArea() {
       // empty string
       value={text}
       // we are making this into a controlled component
-      onChange={(e) => {
-        // we are updating the state of the text, with value of text area
-        // runs when any text is inputted
-        console.log("onChange event fired");
-        console.log(e.target.value);
-        const newText = e.target.value;
-        setText(newText);
-      }}
+      onChange={handleChange}
       className="textarea"
       placeholder="Enter your text here"
       spellCheck="false"
