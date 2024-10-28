cf444fff11bd03b73bdfb4d1b1abeaba6c1f6216
Jacob <jacob.echeverry13@gmail.com>
2024-10-28 15:23:33 -0400

    added conditions for color change if going over word limit

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -14,7 +14,12 @@ export default function Stats({ stats }) {
 function Stat({ label, number }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      {/* added condition for color change */}
+      <span
+        className={`stat__number ${number < 0 ? `stat__number--limit` : ""}`}
+      >
+        {number}
+      </span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
