7487d20ca8465e45c6027c7f1ea1528908ad5d24
Jacob <jacob.echeverry13@gmail.com>
2024-10-28 15:28:12 -0400

    added constants folder to keep hard coded values in

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 export default function Container() {
   const [text, setText] = useState("");
@@ -10,8 +11,8 @@ export default function Container() {
   const stats = {
     numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
     numberOfCharacters: text.length,
-    instagramCharactersLeft: 220 - text.length,
-    facebookCharactersLeft: 2200 - text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
   };
 
   return (
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+// this file is used for our constants such as instagra, amd facebook limits
+
+export const INSTAGRAM_MAX_CHARACTERS = 280;
+export const FACEBOOK_MAX_CHARACTERS = 2200;
