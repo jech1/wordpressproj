1ef867a8a4bd3feecfb376ed525e313aa412977b
Jacob <jacob.echeverry13@gmail.com>
2024-10-31 09:08:59 -0400

    quick prop fix

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -1,6 +1,7 @@
 // here we are creating a functional component called Warning
 // this component takes in a prop called warningText
 // the prop is then displayed in a p tag with a class of warning
+// eslint-disable-next-line react/prop-types
 export default function Warning({ warningText }) {
   return <p className="warning">{warningText}</p>;
 }
